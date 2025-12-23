d87bbc0dca160f8b181f807a3a936faa5de44540
PyotrWrangel <maerlyn@hotmail.it>
2025-12-23 23:25:41 +0100

    aggiunte nuove notizie

---
+++ src/data/datiCard.js
@@ -0,0 +1,93 @@
+
+export const datiCard = [
+    {
+        id: 1,
+        title:  "The Elder Scrolls IV Oblivion.",
+        text: "The Elder Scrolls IV: Oblivion é l'attesissimo quarto capitolo della serie TES prodotto da Bethesda Game Studios per PC e X-Box 360. " +
+        "\nLa storia non presenta collegamenti diretti con quella di Morrowind e si svolgerà nella provincia centrale di Cyrodiil.",
+        image: "/images/forum_join.jpg",
+        logo: "/images/O_logo.gif",
+        date: "2024/12/21",
+        // pinned: true
+    },
+    {
+        id: 2,
+        title:  "TES: IL NUOVO QUESTIARIO",
+        text: "Se pensavate che fossimo stati mangiati da un Daedroth famelico, beh vi sbagliavate! " +
+        "\n\n Eccomi qui dopo tanto tempo per presentarvi il primo frutto di un lavoro molto impegnativo che va avanti da diversi mesi. " +
+        "\n\n Quest'oggi inauguriamo Il Nuovo Questiario di Oblivion. " +
+        "\n\n Si tratta di una versione beta, che a breve amplieremo notevolmente, ma già da ora potrete godere di diversi aspetti interessanti. " +
+        "\n\n 1- Registrati per inserire tu stesso le quest sul sito. " +
+        "\n\n 2- Soluzione di oltre 110 quest presenti nel gioco arricchite da screenshot ed immagini del gioco! " +
+       "\n\n 3- Possibilità di caricare tu stesso i tuoi screenshot. ",
+        image: "/images/nuovo_questiario.jpg",
+        date: "2008/06/09",
+    },
+        {
+        id: 3,
+        title: "THE ELDER SCROLLS ONLINE ??",
+        text: "Facendo un po di ricerche tra i dominii abbiamo notato che ZeniMax Media, la società creatrice della Bethesda Software, " +
+        "ha acquistato recentemente l'indirizzo: ELDERSCROLLSONLINE.COM . Se si associa il fatto che ultimamente hanno aperto una sezione di gioco online, " +
+        "voluta dallo stesso cofondatore Matt Firor, ci siamo chiesti se non sia solo una casualità. " +
+        " Abbiamo contattato la Bethesda per ulteriori chiarimenti, ma non abbiamo ricevuto alcuna risposta ufficiale... fonte: VooDooExtreme - ve3d ",
+        image: "/images/obonline.jpg",
+        imagePosition: "top",
+        date: "2007/12/31",
+    },
+        {
+        id: 4,
+        title:  "NUOVA FUNZIONE CERCA",
+        text: " Continua il progetto del Questiario che grazie all'aiuto un po' di tutti è arrivato alla bellezza di  174 quest e 150 screenshoot del gioco" +
+        " (al momento in cui scrivo). Interessanti aggiunte sono le quest delle Shivering Isles, dell'espansione Knights of the Nine, nonchè numerose quest aggiunte nella sezione Varie." +
+        " ^^ Ho deciso di non levare per il momento il link alle vecchie guide per motivi di indicizzazione." +
+        " Abbiamo attivato inoltre una piccola funzione per cercare tra le quest, che potete trovate direttamente nella colonna qui a destra!",
+        date: "2008/07/19",
+    },
+        {
+        id: 5,
+        title:  "FIGHTER'S STRONGHOLD",
+        text: " Da lunedì, 15 ottobre, è possibile scaricare la versione PC o visitare Xbox Live per la versione Xbox 360 <span class=\"underline text-black-500\">[dal sito Ufficiale di Oblivion.](https://web.archive.org/web/20220330040325/http://www.elderscrolls.com/downloads/updates_plugins.htm)</span>" +
+        "\n<p class=\"font-bold text-center\">FIGHTER'S STRONGHOLD\n DOWNLOAD GRATUITO </p>"  +
+        "<p class=\"text-center\"> Siamo lieti di annunciare che lunedì prossimo, 15 ottobre, ci sarà il rilascio finale dei contenuti scaricabili per Oblivion - Fighter's Stronghold - gratuitamente per una settimana. </p>" +
+        "\n\n Sconfiggi gli attacchi dei marauders e diventa Signore di Battlehorn Castle." +
+        "Situato nel Colovian Highlands ovest di Chorrol, questo enorme Castello vi permette di vivere la vita di un nobile guerriero, quartieri privati, una grande sala da pranzo, cantina, caserme, stanze di allenamento ed altro ancora. 	E quale Castello sarebbe tale senza oscuri e passaggi segreti ??!" +
+        "\n Per ulteriori informazioni su Fighter's Stronghold <span class=\"font-bold underline text-black-500\">[clicca qui.](https://web.archive.org/web/20220330040325/http://dreamwalkers.eu/forums/viewtopic.php?f=2&t=407)",
+        image: "/images/fighter-stronghold.jpg",
+        imagePosition: "top",
+        date: "2007/10/01"
+    },
+        {
+        id: 6,
+        title:  "TORNANO GLI AGGIORNAMENTI",
+        text: " Dopo un periodo di pausa, il sito sta ricominciando pian piano a riempirsi di nuovi contenuti!" +
+        "\nTra le numerose novità che porteranno una nuova ventata nel sito c'è il Nuovo Forum che aprirà stasera e ospiterà vari argomenti." +
+        " Tra i motivi che ci hanno spinto ad abbandonare la comunity di V-Generation, che comunque ringraziamo, c'è la necessita di creare uno spazio 'nostro' adatto ai progetti che svilupperemo ed il fatto che l'accesso al vecchio forum è stato riservato ai soli utenti registrati." +
+        "\n\n Tornando alle guide, ringraziamo Darth Spark che ha scritto veramente molto ultimamente!" +
+        "\n Ecco le ultime novità:" +
+        "\n\n- Gilda dei guerrieri Completata del tutto, da Darth Spark, ebbene si, tutte le gilde sono ora complete :D" +
+        "\n- Aggiunta le prime quattro quest, della <span class=\"font-bold underline text-black-500\">[Quest principale](https://web.archive.org/web/20220330040325/http://www.tes.altervista.org/mainquest.php)</span>",
+        image: "/images/forum_join.jpg",
+        imagePosition: "top",
+        date: "2007/08/19"
+    },
+    //     {
+    //     id: 4,
+    //     title:  "NUOVA FUNZIONE CERCA",
+    //     text: " Continua il progetto del Questiario che grazie all'aiuto un po' di tutti è arrivato alla bellezza di  174 quest e 150 screenshoot del gioco" +
+    //     " (al momento in cui scrivo). Interessanti aggiunte sono le quest delle Shivering Isles, dell'espansione Knights of the Nine, nonchè numerose quest aggiunte nella sezione Varie." +
+    //     " ^^ Ho deciso di non levare per il momento il link alle vecchie guide per motivi di indicizzazione." +
+    //     " Abbiamo attivato inoltre una piccola funzione per cercare tra le quest, che potete trovate direttamente nella colonna qui a destra!",
+    //     date: "2008/07/19",
+    // },
+    //     {
+    //     id: 4,
+    //     title:  "NUOVA FUNZIONE CERCA",
+    //     text: " Continua il progetto del Questiario che grazie all'aiuto un po' di tutti è arrivato alla bellezza di  174 quest e 150 screenshoot del gioco" +
+    //     " (al momento in cui scrivo). Interessanti aggiunte sono le quest delle Shivering Isles, dell'espansione Knights of the Nine, nonchè numerose quest aggiunte nella sezione Varie." +
+    //     " ^^ Ho deciso di non levare per il momento il link alle vecchie guide per motivi di indicizzazione." +
+    //     " Abbiamo attivato inoltre una piccola funzione per cercare tra le quest, che potete trovate direttamente nella colonna qui a destra!",
+    //     date: "2008/07/19",
+    // },
+
+
+]
