b2ab7b833b242a8cac3f8ca75182491bb5e369bf
PyotrWrangel <maerlyn@hotmail.it>
2025-12-23 23:26:20 +0100

    creato file per immagini e descrizione razze

---
+++ src/data/races.js
@@ -0,0 +1,72 @@
+export const Races = [
+  {
+    id: 1,
+    image: "/images/Orc.webp",
+    name: "ORCHI",
+    description:
+      "Guerrieri implacabili, famosi per forza, resistenza e disciplina tribale."
+  },
+  {
+    id: 2,
+    image: "/images/Breton1.webp",
+    name: "BRETONI",
+    description:
+      "Abili nella magia e intelligenti, combinano talento arcano e determinazione."
+  },
+  {
+    id: 3,
+    image: "/images/WoodElf.webp",
+    name: "ELFI DEI BOSCHI (BOSMER)",
+    description:
+      "Agili e legati alla natura, eccellono nel tiro con l’arco e nella furtività."
+  },
+  {
+    id: 4,
+    image: "/images/Khajiit1.webp",
+    name: "KHAJIIT",
+    description:
+      "Felinidi agili e furtivi, perfetti come ladri, mercanti e esploratori."
+  },
+  {
+    id: 5,
+    image: "/images/Nord.webp",
+    name: "NORDICI",
+    description:
+      "Forti e coraggiosi, nati per il freddo e per la battaglia corpo a corpo."
+  },
+  {
+    id: 6,
+    image: "/images/Imperial.webp",
+    name: "IMPERIALI",
+    description:
+      "Diplomatici carismatici e strategici, eccellono nel comando e nel commercio."
+  },
+  {
+    id: 7,
+    image: "/images/Altmer1.webp",
+    name: "ELFI ALTI (ALTMER)",
+    description:
+      "Padroni della magia, orgogliosi e potenti, con enorme talento arcano."
+  },
+  {
+    id: 8,
+    image: "/images/Argonian.webp",
+    name: "ARGONIANI",
+    description:
+      "Rettiliani adattabili e resistenti, perfetti per ambienti ostili e paludosi."
+  },
+  {
+    id: 9,
+    image: "/images/Redguard1.webp",
+    name: "GUARDIE ROSSE",
+    description:
+      "Guerrieri esperti e veloci, maestri nell’uso delle armi e nel combattimento."
+  },
+  {
+    id: 10,
+    image: "/images/Dunmer.webp",
+    name: "ELFI OSCURI (DUNMER)",
+    description:
+      "Fieri e misteriosi, bilanciano abilità magiche con grande talento marziale."
+  }
+];
