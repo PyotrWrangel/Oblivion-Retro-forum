7413d8a1391c687592bd806bdf762d154305e360
PyotrWrangel <maerlyn@hotmail.it>
2026-05-19 17:59:58 +0200

    updated styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import Homepage from './pages/Homepage';
 import Header from './components/Header';
 import Razze from './pages/Razze';
+import Class from './pages/Class';
+import Age from './pages/Age';
 
 
 function App() {
@@ -14,15 +16,9 @@ function App() {
     <Routes>
       <Route path='/' element={<Homepage />} />
       <Route path='/races' element={<Razze />} />
-      {/* 
-      <Route path='/' element={<HomePage />} />
-      <Route path='/' element={<HomePage />} />
-      <Route path='/' element={<HomePage />} />
-      <Route path='/' element={<HomePage />} />
-      <Route path='/' element={<HomePage />} />
-      <Route path='/' element={<HomePage />} />
-      <Route path='/' element={<HomePage />} />
-      <Route path='/' element={<HomePage />} /> */}
+      <Route path='/classes' element={<Class />} />
+      <Route path='/ages' element={<Age />} />
+
     </Routes> 
     </>
   )
