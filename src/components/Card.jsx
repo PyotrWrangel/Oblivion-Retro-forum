cd58b03b000e0ee5b9b7d8687f12a75de8c281c0
PyotrWrangel <maerlyn@hotmail.it>
2026-05-19 17:56:22 +0200

    modified style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,23 +7,23 @@ function Card ({ title, text, image, imgTitle, logo, imagePosition = "bottom", d
         return (
         <>
 
-        <div className="mx-auto pt-2 bg-[#dacca3] w-120 mt-[20px] md-[20px] border-4 border-[#5b4732] ring-4 ring-[#3a2a1a] ring-offset-4 ring-offset-[#dacca3] p-4">
+        <div className="mx-auto pt-2 bg-[#dacca3] w-250 mt-[20px] mb-[100px] md-[20px] border-4 border-[#5b4732] ring-4 ring-[#3a2a1a] ring-offset-4 ring-offset-[#dacca3] p-4">
             <div className=" bg-[#68513a]">
-                <p className="font-serif text-center font-[700] text-[16px] text-[#EEE2BF]">{ title }</p>
+                <p className="font-serif text-center font-[700] text-[30px] text-[#EEE2BF]">{ title }</p>
                 </div>
                  {imagePosition === "top" &&
-                <img className="pt-1 pl-[20px] px-[20px] max-w-[450px] m-auto pb-[15px]" src={image} alt={imgTitle}></img>}
+                <img className="pt-1 pl-[20px] px-[20px] w-[500px] m-auto pb-[15px]" src={image} alt={imgTitle}></img>}
                 <div className="flex flex-cols-2 m-2"> 
                     {logo &&
-                   <img src={logo} alt="oblivion logo"></img>}
-                   <div className="text-justify px-2 pr-8 text-[#5b4732] text-[13px] leading-tight whitespace-pre-line">
+                   <img src={logo} alt="oblivion logo" className="w-30"></img>}
+                   <div className="text-justify px-2 pr-8 text-[#5b4732] text-[22px] leading-tight whitespace-pre-line">
                    <ReactMarkdown rehypePlugins={[rehypeRaw]}>{ text }</ReactMarkdown>
                    </div>
                 </div>
                 {/* posizionamento immagine */}
                 {imagePosition === "bottom" && 
-                <img className="max-w-[450px] m-auto" src={image} alt={imgTitle}></img>}
-                <p className="text-sm text-left p-2 text-[13px] prose">{date}</p>
+                <img className="max-w-[550px] m-auto" src={image} alt={imgTitle}></img>}
+                <p className="text-sm text-left p-2 text-[16px] prose">{date}</p>
             </div>
         
         </>
