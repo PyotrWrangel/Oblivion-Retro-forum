c06f662047772a9a07cdbf81b5da8fedf6ccdc22
PyotrWrangel <maerlyn@hotmail.it>
2026-05-20 16:54:22 +0200

    upgaded component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,22 +7,22 @@ function Card ({ title, text, image, imgTitle, logo, imagePosition = "bottom", d
         return (
         <>
 
-        <div className="mx-auto pt-2 bg-[#dacca3] w-250 mt-[20px] mb-[100px] md-[20px] border-4 border-[#5b4732] ring-4 ring-[#3a2a1a] ring-offset-4 ring-offset-[#dacca3] p-4">
+        <div className="pt-2 bg-[#dacca3] w-200 mt-[20px] mb-[50px] md-[20px] border-4 border-[#5b4732] ring-4 ring-[#3a2a1a] ring-offset-4 ring-offset-[#dacca3] p-4">
             <div className=" bg-[#68513a]">
-                <p className="font-serif text-center font-[700] text-[30px] text-[#EEE2BF]">{ title }</p>
+                <p className="font-serif text-center font-[700] text-[26px] text-[#EEE2BF]">{ title }</p>
                 </div>
                  {imagePosition === "top" &&
                 <img className="pt-1 pl-[20px] px-[20px] w-[500px] m-auto pb-[15px]" src={image} alt={imgTitle}></img>}
                 <div className="flex flex-cols-2 m-2"> 
                     {logo &&
                    <img src={logo} alt="oblivion logo" className="w-30"></img>}
-                   <div className="text-justify px-2 pr-8 text-[#5b4732] text-[22px] leading-tight whitespace-pre-line">
+                   <div className="text-left px-2 pr-8 text-[#5b4732] text-[18px] leading-tight whitespace-pre-line">
                    <ReactMarkdown rehypePlugins={[rehypeRaw]}>{ text }</ReactMarkdown>
                    </div>
                 </div>
                 {/* posizionamento immagine */}
                 {imagePosition === "bottom" && 
-                <img className="max-w-[550px] m-auto" src={image} alt={imgTitle}></img>}
+                <img className="w-[500px] max-w-[750px] m-auto" src={image} alt={imgTitle}></img>}
                 <p className="text-sm text-left p-2 text-[16px] prose">{date}</p>
             </div>
         
